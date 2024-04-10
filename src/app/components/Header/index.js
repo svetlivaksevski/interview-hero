0edf69ba57557b8f7fcd0084f004ae5647c1860c
svetlivaksevski <svetlivaksevski@gmail.com>
2024-04-10 23:30:01 +0200

    Add alt to the images

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Header() {
   return (
     <div className="header">
-      <Image src={Logo} width={250} />
+      <Image src={Logo} width={250} alt="Logo" />
     </div>
   );
 }
