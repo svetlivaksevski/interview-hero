ffa92bb35a058b60ce6ae43e44fe969d3d4a164e
svetlivaksevski <svetlivaksevski@gmail.com>
2024-04-10 23:25:32 +0200

    Solved bug

--- src/app/page.js
+++ src/app/page.js
@@ -13,9 +13,9 @@ export default function Home() {
           Prepare better for your next interview. Just in one click!
         </p>
         <p className="introtext">
-          Interview hero is a platform where you can add questions you've been
-          ask during an interivew and get useful tips from our users. There are
-          no more difficult quesitons.
+          Interview hero is a platform where you can add questions you&apos;ve
+          been ask during an interivew and get useful tips from our users. There
+          are no more difficult quesitons.
         </p>
       </section>
       <Randomquestion />
