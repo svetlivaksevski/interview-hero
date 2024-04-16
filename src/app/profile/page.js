3f5990a3ea621a215930845e156ad790bbc4c61c
svetlivaksevski <svetlivaksevski@gmail.com>
2024-04-16 15:23:15 +0200

    add how many question you added

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -4,10 +4,13 @@ import Navigation from "@/components/Navigation";
 import Header from "@/components/Header";
 import SignInPage from "@/components/SignInPage";
 import { signIn, useSession, signOut } from "next-auth/react";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function Profile() {
   const session = useSession();
-  console.log(session);
+
   if (session.status === "loading") {
     return null;
   }
@@ -19,11 +22,22 @@ export default function Profile() {
   if (session.status === "authenticated") {
     const creator = session.data?.user?.id;
 
+    const { data, error, isLoading } = useSWR("api/question", fetcher);
+
+    if (error) return <div>Failed to load</div>;
+    if (isLoading) return <div>Loading graffiti added by me...</div>;
+    const questionsAddedbyYou = data?.filter(
+      (info) => info.userId === session.data.user.userId
+    );
+
     return (
       <>
         <Header />
         <img src={session.data.user.image} className="profilepicture" />
         <h1>You sing-up as {session.data.user.name}</h1>
+
+        <p>Questions you added: {questionsAddedbyYou.length}</p>
+        <p></p>
         <button onClick={signOut}>Sign Out</button>
         <Navigation />
 
