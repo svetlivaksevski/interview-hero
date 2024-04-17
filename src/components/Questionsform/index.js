ce04057e8aa3915135b2c5b49856b6df58aece0e
svetlivaksevski <svetlivaksevski@gmail.com>
2024-04-17 11:07:16 +0200

    Successfull and error messages

--- src/components/Questionsform/index.js
+++ src/components/Questionsform/index.js
@@ -2,24 +2,27 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { signIn, useSession, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
-export default function QuestionsForm({
-  onSubmit,
-
-  defaultData,
-  formName,
-  userName,
-}) {
+export default function QuestionsForm({ defaultData }) {
   const router = useRouter();
   const session = useSession();
   const [loadingAddQuestion, setLoadingAddQuestion] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [failedSubmission, setfailedSubmission] = useState("");
 
   async function handleSubmit(event) {
     event.preventDefault();
     setLoadingAddQuestion(true);
     const formData = new FormData(event.target);
     const entryData = Object.fromEntries(formData);
+
+    if (entryData.select === "No category assigned") {
+      setLoadingAddQuestion(false);
+      setErrorMessage("Please select a category.");
+      return;
+    }
     const questionWithUser = { ...entryData, userId: session.data.user.userId };
     AddQuestion(questionWithUser);
   }
@@ -33,15 +36,23 @@ export default function QuestionsForm({
       body: JSON.stringify(entryData),
     });
     if (!response.ok) {
-      alert("Ooops, something went wrong. Your question was not added :(");
-      console.log(error);
+      setfailedSubmission(
+        "Ooops, something went wrong. Your question was not added :("
+      );
     }
     if (response.ok) {
       setLoadingAddQuestion(false);
-      router.push("/question");
-      alert("Yes! You have added this question. See the question down below!");
+      setErrorMessage("");
+      setSuccessMessage(
+        "Yes! You have added this question. See the question down below!"
+      );
+      setfailedSubmission("");
+      setTimeout(() => {
+        router.push("/question");
+      }, 1000);
     }
   }
+
   return (
     <div>
       <form className="form" aria-labelledby="Form" onSubmit={handleSubmit}>
@@ -63,7 +74,28 @@ export default function QuestionsForm({
           cols="30"
           defaultValue={defaultData?.answer}
         />
+        <label htmlFor="select">Select question category:</label>
+        <div className="custom-select">
+          <select id="select" name="select">
+            <option value="No category assigned">-- Select category --</option>
+            <option value="Behavioral & Cultural Fit">
+              Behavioral & Cultural Fit
+            </option>
+            <option value="Technical">Technical</option>
+            <option value="Compensation and Benefits">
+              Compensation and Benefits
+            </option>
+            <option value="Professional Development and Growth">
+              Professional Development and Growth
+            </option>
+            <option value="Problem-Solving">Problem-Solving</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
         <button type="submit">Submit</button>
+        {errorMessage && <p>{errorMessage}</p>}
+        {successMessage && <p>{successMessage}</p>}
+        {failedSubmission && <p>{failedSubmission}</p>}
       </form>
     </div>
   );
