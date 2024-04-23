395803708be0d48cfb75f09c2d8803b9d0e390c9
svetlivaksevski <svetlivaksevski@gmail.com>
2024-04-23 12:14:42 +0200

    Add changes

--- src/app/question/[id]/page.js
+++ src/app/question/[id]/page.js
@@ -9,6 +9,8 @@ import Comments from "@/components/Comments";
 import CommentForm from "@/components/CommentForm";
 import { LiaEdit } from "react-icons/lia";
 import { LiaTrashAltSolid } from "react-icons/lia";
+import { FaRegStar } from "react-icons/fa";
+import { FaStarHalfAlt } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
@@ -53,7 +55,7 @@ export default function QuestionPage({ params }) {
   const rating = data.ratedByUserId.map((item) => item.rating);
   const sumOfRatings = rating.reduce((acc, rating) => acc + rating, 0);
   const averageRating = sumOfRatings / rating.length;
-  const roundedAverageRating = Math.round(averageRating);
+  const roundedAverageRating = averageRating.toFixed(1);
 
   async function handleRating(e) {
     e.preventDefault();
@@ -84,12 +86,16 @@ export default function QuestionPage({ params }) {
   return (
     <>
       <Header />
-      <span onClick={() => window.history.back()}>← Go back</span>
+      {/* <span onClick={() => window.history.back()}>← Go back</span> */}
       <div className="container-questions">
         <div className="questions-data">
           <div className="user-info">
-            <img src={data.profileImage} className="imagequestion" />
             <div className="user-info-text">
+              <img src={data.profileImage} className="imagequestion" />
+              <p className="text-question">
+                {data.userName} is just wondering: How would you handle this
+                question?
+              </p>
               <div className="buttons-question">
                 {session?.user.userId === data?.userId ? (
                   <>
@@ -104,10 +110,6 @@ export default function QuestionPage({ params }) {
                   <p></p>
                 )}
               </div>
-              <p className="text-question">
-                {data.userName} is just wondering: How would you handle this
-                question?
-              </p>
             </div>
           </div>
           <h2>Your question:</h2>
@@ -130,31 +132,46 @@ export default function QuestionPage({ params }) {
               </p>
               <p className={color}>{data.difficulty}</p>
             </div>
-            <form onSubmit={handleRating}>
-              {[1, 2, 3, 4, 5].map((star) => (
-                <label key={star}>
-                  <input
-                    type="radio"
-                    name="rating"
-                    value={star}
-                    onChange={handleRating}
-                    defaultChecked={roundedAverageRating === star}
-                    style={{ display: "none" }}
-                  />
-                  <FaStar
-                    size={24}
-                    color={roundedAverageRating >= star ? "#ffc107" : "#fcfcfc"}
-                    fill={roundedAverageRating >= star ? "#ffc107" : "#fcfcfc"}
-                    style={{ cursor: "pointer" }}
-                  />
-                </label>
-              ))}
-              <button
-                id="ratingForm"
-                style={{ display: "none" }}
-                onClick={handleRating}
-              ></button>
-            </form>
+            <div className="box">
+              {session && (
+                <>
+                  <form onSubmit={handleRating}>
+                    {[1, 2, 3, 4, 5].map((star) => (
+                      <label key={star}>
+                        <input
+                          type="radio"
+                          name="rating"
+                          value={star}
+                          onChange={handleRating}
+                          defaultChecked={roundedAverageRating === star}
+                          style={{ display: "none" }}
+                        />
+                        {star <= roundedAverageRating ? (
+                          <span role="img" aria-label="Star">
+                            <FaStar fill="#fbb117" />
+                          </span>
+                        ) : star - roundedAverageRating === 0.5 ? (
+                          <span>
+                            <FaStarHalfAlt fill="#fbb117" />
+                          </span>
+                        ) : (
+                          <span role="img" aria-label="Empty Star">
+                            <FaRegStar />
+                          </span>
+                        )}
+                      </label>
+                    ))}
+                    <button
+                      id="ratingForm"
+                      style={{ display: "none" }}
+                      onClick={handleRating}
+                    ></button>
+                  </form>
+                  <p>({rating.length === 0 ? 0 : roundedAverageRating})</p>/
+                  <p>{rating.length}</p>
+                </>
+              )}
+            </div>
           </div>
         </div>
 
