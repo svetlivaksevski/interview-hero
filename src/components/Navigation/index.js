fdb62c11957252bbc1f26dbb478ede8e6dd93d76
svetlivaksevski <svetlivaksevski@gmail.com>
2024-04-25 12:19:51 +0200

    Styling

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -9,25 +9,25 @@ export default function Navigation() {
   return (
     <ul className="navigation">
       <li className="navbuttons">
-        <a href="/">
+        <a className="nav-icons-text" href="/">
           <LiaHomeSolid fontSize={25} />
           <span> Home</span>
         </a>
       </li>
       <li className="navbuttons">
-        <a href="/create">
+        <a className="nav-icons-text" href="/create">
           <LiaPlusCircleSolid fontSize={25} />
           <span>Add question</span>
         </a>
       </li>
       <li className="navbuttons">
-        <a href="/question">
+        <a className="nav-icons-text" href="/question">
           <LiaListSolid fontSize={25} />
           <span> All questions</span>
         </a>
       </li>
       <li className="navbuttons">
-        <a href="/profile">
+        <a className="nav-icons-text" href="/profile">
           <LiaUserCircle fontSize={25} />
 
           <span> Profile</span>
